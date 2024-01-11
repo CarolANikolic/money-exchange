75c5f38e69ee78c60645bfebbc1c230022458bd3
Carol A. Nikolic <carolnikolic@gmail.com>
2024-01-11 14:08:24 +0100

    feat(server): set-up view engine to ejs and path to client directory in order to access and load static files.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,19 @@
 import express from "express";
 import axios from "axios";
+import { fileURLToPath } from "url";
+import path from "path";
 
 const app = express();
 const port = 3000;
+
+const __fileName = fileURLToPath(import.meta.url);
+const __clientDir = path.resolve(__fileName, "../../client");
+app.set("view engine", "ejs");
+app.set("views", path.join(__clientDir, "views"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}.`)
