3008844ea052321279c718f8489419b60d93aa90
Carol A. Nikolic <carolnikolic@gmail.com>
2024-01-12 09:56:24 +0100

    feat: display currencies, including default selected currency and code.

--- server/app.js
+++ server/app.js
@@ -5,14 +5,28 @@ import path from "path";
 
 const app = express();
 const port = 3000;
+const API_URL = "api.frankfurter.app";
 
 const __fileName = fileURLToPath(import.meta.url);
 const __clientDir = path.resolve(__fileName, "../../client");
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__clientDir, "views"));
+app.use(express.static(__clientDir));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs")
+
+app.get("/", async (req, res) => {
+    try {
+        const response = await axios.get(`https://${API_URL}/currencies`);
+        const currenciesObject = response.data;
+        const currenciesArray = Object.entries(currenciesObject).map(([code, name]) => ({ code, name }));
+
+        res.render("index.ejs", { currencies: currenciesArray });
+    } catch (error) {
+        console.log("Error fetching currencies:", error);
+        res.status(500).send("Internal server error")
+
+    }
 })
 
 app.listen(port, () => {
